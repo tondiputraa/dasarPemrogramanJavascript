72625d733e6742c2810b4b6d734625f1fc5e3891
tondiputraa <tondiputra02@gmail.com>
2024-01-10 16:07:57 +0700

    menambahkan confirm

--- popup.js
+++ popup.js
@@ -3,4 +3,17 @@
 // alert('saya');
 // alert('Tondi Putra');
 
-var nama = prompt('masukkan nama:');
+// var nama = prompt('masukkan nama:');
+// alert(nama);
+
+// confirm('apakah kamu yakin?');
+var confirm = confirm('apakah kamu yakin?');
+alert(confirm);
+
+if (confirm===true){
+    alert('user menekan tombol OK!');
+}
+else 
+{
+    alert('User menekan tombol CANCEL!');
+}
