404af32b766e2d084c5964f9e464069f5b17d684
tondiputraa <tondiputra02@gmail.com>
2024-01-11 12:40:55 +0700

    mengedit console log pengulangan

---
+++ pengulangan.js
@@ -0,0 +1,15 @@
+// console.log('Hello world!');
+// console.log('Hello world!');
+// console.log('Hello world!');
+// console.log('Hello world!');
+// console.log('Hello world!');
+// console.log('Hello world!');
+// console.log('Hello world!');
+// console.log('Hello world!');
+// console.log('Hello world!');
+// console.log('Hello world!');
+
+while (true)
+{
+    console.log('Hello world!');
+}
