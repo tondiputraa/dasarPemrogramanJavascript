4edf79919d658411d78039d82dd5b69f1ac35436
tondiputraa <tondiputra02@gmail.com>
2024-01-11 13:28:11 +0700

    membuat pengulangan angkot1.js

--- angkot1.js
+++ angkot1.js
@@ -1,7 +1,8 @@
-var nilaiAwal = 1;
-var kondisi = prompt('Jumlah Pengulangan');
-while (nilaiAwal<=kondisi)
+// var kondisi = prompt('Jumlah Pengulangan');
+var noAngkot = 1;
+var jmlhAngkot=10;
+while (noAngkot<=jmlhAngkot)
 {
-    console.log('Angkot No.'+nilaiAwal+' beroperasi dengan baik.');
-    nilaiAwal++;
+    console.log('Angkot No.'+noAngkot+' beroperasi dengan baik.');
+    noAngkot++;
 }
