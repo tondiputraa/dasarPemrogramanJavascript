7174c7dfb4a7d5657553f620a92e3a133a67142d
tondiputraa <tondiputra02@gmail.com>
2024-01-11 12:49:40 +0700

    membuat pengulangan dengan kondisi nilaiAwal

--- pengulangan.js
+++ pengulangan.js
@@ -8,9 +8,16 @@
 // console.log('Hello world!');
 // console.log('Hello world!');
 // console.log('Hello world!');
-var ulang = true;
-while (ulang)
+// var ulang = true;
+// while (ulang)
+// {
+//     console.log('Hello world!');
+//     ulang = confirm('Lagi?');
+// }
+
+var nilaiAwal = 1;
+while (nilaiAwal<=5)
 {
     console.log('Hello world!');
-    ulang = confirm('Lagi?');
+    nilaiAwal++;
 }
