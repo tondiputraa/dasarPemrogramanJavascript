193395e94e4778e20631c01aeed88de19ba48c77
tondiputraa <tondiputra02@gmail.com>
2024-01-11 16:42:43 +0700

    membuat angkot3yt.js

---
+++ angkot3.js
@@ -0,0 +1,14 @@
+var jmlhAngkot=10;
+var angkotBeroperasi=5;
+var noAngkot=1;
+
+
+for(noAngkot=1;noAngkot<=jmlhAngkot;noAngkot++)
+if(noAngkot<=angkotBeroperasi)
+{
+    console.log('Angkot No. '+noAngkot+' beroperasi dengan Baik.');
+}
+else
+{
+    console.log('Angkot No. '+noAngkot+' Tidak beroperasi dengan Baik.');
+}
---
+++ angkot3yt.js
@@ -0,0 +1,17 @@
+var jmlhAngkot=10;
+var angkotBeroperasi=6;
+
+for(var noAngkot=1;noAngkot<=jmlhAngkot;noAngkot++)
+{
+
+    if(noAngkot<=6)
+    {
+        console.log('Angkot No. ' + noAngkot +' Beroperasi dengan baik.');
+    }
+else
+{
+    console.log('Angkot No. '+noAngkot+' Sedang tidak beroperasi.');
+}
+
+
+}
