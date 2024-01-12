b568d5f3433811a643ee7d9d4ea1a9c0f60c4bcd
tondiputraa <tondiputra02@gmail.com>
2024-01-12 19:48:20 +0700

    membuat function penjumlahan dua buah bilangan

---
+++ functionJumlahDuabilangan.js
@@ -0,0 +1,19 @@
+function jumlahDuaBilangan(a,b)
+{
+    var total;
+    total=a+b;
+    return total;
+}
+
+console.log(jumlahDuaBilangan(233,33));
+
+console.log(jumlahDuaBilangan(22,312));
+
+console.log(jumlahDuaBilangan(2,3232));
+
+console.log(jumlahDuaBilangan(23,53));
+
+console.log(jumlahDuaBilangan(1,76));
+
+console.log(jumlahDuaBilangan(8,50));
+alert(jumlahDuaBilangan(2,3));;
