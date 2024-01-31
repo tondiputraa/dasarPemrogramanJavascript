406400dc264f6dc7f6fee3c7158bd7beb1dff5d2
tondiputraa <tondiputra02@gmail.com>
2024-01-31 16:38:05 +0700

    membuat program faktorial (rekursif)

--- refactoringDanRekursif/rekursif.js
+++ refactoringDanRekursif/rekursif.js
@@ -5,7 +5,20 @@
 // tes();
 
 
-for (var i=10;i>0;i--){
+// for (var i=10;i>0;i--){
 
-    console.log(i);
+//     console.log(i);
+// }
+
+function tampilAngka(n) {
+    if(n===0) return;       //Base case (statment buat memberhentikan) 
+    console.log(n);
+    tampilAngka(n-1);
 }
+
+function faktorial(f) {
+    if(f===0) return 1;
+    return f * faktorial(f-1);
+}
+
+console.log(faktorial(6));
