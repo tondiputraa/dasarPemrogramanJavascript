4c5dca76ef821f84749d2852b870afd627b09505
tondiputraa <tondiputra02@gmail.com>
2024-02-01 16:32:26 +0700

    membuat file manipulasiArray.js, length, join, pop n push, unshift an shift

---
+++ Array/manipulasiArray.js
@@ -0,0 +1,76 @@
+// Manipulasi array
+
+// 1 . Menambahkan isi array
+
+// var binatang = [];
+// binatang[0] = 'kucing';
+// binatang[1] ='sapi';
+// binatang[2] = 'kelinci';
+// binatang[3] = 'koala';
+
+// console.log(binatang);
+
+// Menghapus Isi Array
+
+// var binatang = ['kucing' , 'sapi' , 'kelinci' , 'panda' , 'koala'];
+// binatang[2]='naga';
+
+// console.log(binatang);
+
+// 3 . Menampilkan isi Array
+
+// var binatang = ['kucing' , 'sapi' , 'kelinci' , 'panda' , 'koala','ular'];
+
+// for (var i=0; i<binatang.length;i++) {
+//     console.log('Binatang '+(i+1)+': ' +binatang[i])
+// }
+
+
+// 4. Method Pada Array:
+
+
+var binatang = ['kucing' , 'sapi' , 'kelinci' , 'panda' , 'koala'];
+//    1. Join
+// console.log(binatang.join(' - '));
+
+//    2. Push and Pop
+//  binatang.push('kambing','semut','jerapah');
+
+//  binatang.pop();
+//  binatang.pop();
+//  console.log(binatang.join(' - '));
+
+//  3. unshift dan shift
+//    binatang.unshift('ulat'); // ----> menambahkan elemen PERTAMA (Unshift)
+    
+   binatang.shift();
+   console.log(binatang.join(' - '));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
