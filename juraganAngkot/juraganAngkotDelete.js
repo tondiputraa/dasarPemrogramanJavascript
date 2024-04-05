c7a8ad920be893c7032cc3c454e234431cfac752
tondiputraa <tondiputra02@gmail.com>
2024-04-05 18:04:35 +0700

    juraganAngkotDelete.js

---
+++ juraganAngkot/juraganAngkotDelete.js
@@ -0,0 +1,35 @@
+var penumpang = ['tondi','putra',undefined,'dian'];
+var hapusPenumpang = function (namaPenumpang, penumpang)
+{
+    //Jika angkot kosong
+    if(penumpang.length==0)
+    {
+        //Tampilkan Pesan
+        console.log('Angkot kosong!!');
+        }
+        //else
+        else
+        {
+
+            //Telusuri Seluruh Kursi dari Awal
+            for (var i=0;i<penumpang.length;i++){
+                //Jika ada kursi kosong
+                if (penumpang[i]==namaPenumpang){
+                    // Tambah penumpang undefined di kursi tersebut
+                    penumpang[i]=undefined;
+                    // Kembalikan isi array dan keluar dari function
+                    return penumpang;
+            }
+            
+        
+        }
+        //jika tidak ada nama yang sama
+        
+            // tampilkan pesan kesalahannya
+            console.log('Penumpang '+namaPenumpang+' tidak berada di Angkot !!!');
+            return penumpang;
+
+
+      }
+
+}
