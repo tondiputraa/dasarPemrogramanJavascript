8341f54b408e11c524bd6be23636648be589565e
tondiputraa <tondiputra02@gmail.com>
2024-04-13 20:46:18 +0700

    membuatobject2

---
+++ javascriptLanjutan/objectLanjutan.js
@@ -0,0 +1,54 @@
+//1 object literal
+// let mahasiswa = {
+// nama: 'tondi',
+// energi: 10,
+// makan: function(porsi){
+//     this.energi=this.energi+(porsi*5);
+    // console.log(`Halo ${this.nama} Selamat Makan!\nEnergi kamu menjadi=${this.energi}`);
+
+// }}
+
+// 2.function declaration
+
+// function Mahasiswa (nama,energi){
+// let mahasiswa = {};
+// mahasiswa.nama = nama;
+// mahasiswa.energi= energi;
+
+// mahasiswa.makan=function(porsi
+// ){
+// this.energi+=porsi;    
+// console.log(`Halo ${this.nama} Selamat makan!`);
+// }
+
+// mahasiswa.main=function(jam){
+//     this.energi-=jam;
+//     console.log(`Halo ${this.nama} \nEnergi kamu menjadi=${this.energi}`);
+// }
+// return mahasiswa; //penting buat return!
+
+// }
+
+// let tondi = Mahasiswa('tondi',20);
+
+//3.constructor function
+function Mahasiswa (nama,energi){
+    // let mahasiswa = {}; //dihapus kalau disini
+    this.nama = nama;
+    this.energi= energi;
+    
+    this.makan=function(porsi
+    ){
+    this.energi+=porsi;    
+    console.log(`Halo ${this.nama} Selamat makan!`);
+    }
+    
+    this.main=function(jam){
+        this.energi-=jam;
+        console.log(`Halo ${this.nama} \nEnergi kamu menjadi=${this.energi}`);
+    }
+    // return mahasiswa; //dihapus kalau disini
+    
+    }
+    
+    let tondi = new Mahasiswa('tondi',20); // wajib pake new
---
+++ javascriptLanjutan/objectLanjutan2.js
@@ -0,0 +1,121 @@
+// //4.object create();
+
+//const namaObject ={preperti:blabala,properti2:blabla...};---->rumus const
+//     const methodMahasiswa ={
+//     makan:function(porsi)
+    
+//     {
+//     this.energi+=porsi;    
+//     console.log(`Halo ${this.nama} Selamat makan!`);
+//     },
+    
+//     main:function(jam){
+//         this.energi-=jam;
+//         console.log(`Halo ${this.nama} \nEnergi kamu menjadi=${this.energi}`);
+//     },
+
+//     tidur:function(jam){
+// this.energi=+jam*2;
+// console.log(`Halo ${this.nama} Selamat Tidur! \nEnergi kamu menjadi=${this.energi}`);
+
+//     }
+// };
+
+
+//     function Mahasiswa (nama,energi){
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi= energi; 
+    
+//     return mahasiswa; 
+//     }
+    
+//     let tondi = new Mahasiswa('tondi',20); // wajib pake new
+
+
+
+
+class Mahasiswa {
+    constructor(nama,energi){
+        this.nama=nama;
+        this.energi=energi;
+        }    
+        makan(porsi){
+        this.energi+=porsi;
+        return `selamat makan ${this.nama} energi anda menjadi ${this.energi}`;
+
+        }
+
+        main(jam){
+        this.energi-=jam;
+        return `Halo ${this.nama} \nEnergi kamu menjadi= ${this.energi}`;
+        }
+
+        tidur(jam){
+        this.energi+=jam*2;
+        return `Halo ${this.nama} Selamat Tidur! \nEnergi kamu menjadi= ${this.energi}`;
+        }
+    
+    }
+
+    let tondi = new Mahasiswa('tondi',20); // wajib pake new
+// const methodMahasiswa = {
+// makan:function (porsi){
+//     this.energi=+porsi;
+//     console.log(`Selamat makan ${this.nama} energi kamu menjadi ${this.energi}`);
+//     },
+// main:function(jam){
+//     this.energi=-jam;
+//     console.log(`Selamat bermain ${this.nama} energi kamu menjadi ${this.energi}`);
+
+// },
+// tidur:function (jam) {
+// this.energi=-jam*2;
+// console.log(`Selamat tidur ${this.nama} energi kamu menjadi ${this.energi}`);
+
+
+// }
+
+// };
+
+// function Mahasiswa (nama,energi) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+//     return mahasiswa;
+// }
+
+// let tondi = new Mahasiswa ('tondi',20);
+
+// const methodMahasiswa = {
+//     makan:function(porsi){
+//         this.energi+=porsi;
+//     },
+
+//     main:function (jam){
+//         this.energi-=jam;
+
+//     },
+
+//     tidur:function(jam){
+//         this.energi+=jam*2;
+//     }
+
+
+// };
+// function Mahasiswa (nama,energi){
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama=nama;
+//     mahasiswa.energi=energi;
+//     return mahasiswa;
+// }
+
+// let tondi = new Mahasiswa ('tondi',2);
+
+
+
+
+
+
+
+
